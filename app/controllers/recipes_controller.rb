f554de05ad29ab709421838f75187027e6007736
Anna Costalonga <annamaria.costalonga@hpi.de>
2022-11-27 19:52:08 +0100

    Reset params after results are fetched
    This is necessary to avoid malformed query params, once the
    results are obtained in the controller.
    A small Stimulus sprinkle removes also params from the URL, while typing in the searchbox.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,14 +2,17 @@
 
 class RecipesController < ApplicationController
   def index
-    @params_present = false
-    @search_params = params[:query]
+    @params_present = false # This will return useful in the views
+    @search_params = params[:q]
 
-    if @search_params
+    if @search_params.present?
       @recipes = Recipe.search_text(@search_params).order(:title).page params[:page]
       @recipes_count = @recipes.total_count
       @params_present = true
+      # Reset the params to be able to accept new terms and avoid malformed queries, ie. StatementInvalid errors
+      @search_params = nil
     else
+      # Attention! `sample` returns an array and not an AR relation
       @recipes = Recipe.where('ratings > ?', 2.0).sample(6)
     end
   end
@@ -21,6 +24,6 @@ class RecipesController < ApplicationController
   private
 
   def search_params
-    params.permit(:query)
+    params.permit(:q)
   end
 end
